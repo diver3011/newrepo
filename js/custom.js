a7d024b9cfec1ccab22c96a691e52ac35188e06a
Ahmed Fateh <diver30111@gmail.com>
2019-12-09 17:46:08 +0200

    initial commit

---
+++ js/custom.js
@@ -0,0 +1,120 @@
+/*global $, $ , window , body, html*/
+
+$(function () {
+    
+    'use strict';
+    
+//NiceScroll    
+    
+    $("body").niceScroll({
+        cursorcolor: "#4182D4",
+        cursorwidth: "7px",
+        cursorborder: "1px solid transparent"
+
+        
+    });
+      
+    
+   
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+ $('.navbar li a').click(function (e) {
+        
+        e.preventDefault();
+        
+        $('body,html').animate({
+            
+            
+            scrollTop: $('#' + $(this).data('scroll')).offset().top
+            
+            
+        }, 2000);
+        
+        
+        
+        
+        
+        
+        
+    });
+    */
